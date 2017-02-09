5ad11d8253934c50645a200e2c519c06fba05b16
Aurelien Roux <aureroux13@me.com>
2017-02-09 17:05:51 -0500

    managed to fetch data from api

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&no
 
 var FLICKR_API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key='
 var FLICKR_API_KEY = '0e20370658ae9b1a32d1f24c552061f8';
+var FLICKR_API_SEARCHTERM = 'guitar'
+
+
+
+function myFunction(){
+	var url = `${FLICKR_API_URL}${FLICKR_API_KEY}&text=${FLICKR_API_SEARCHTERM}`;
+
+	return(
+		fetch(url)
+		.then(result => result.json())
+		.then(data => console.log(data))
+		
+	)
+	
+}
+
