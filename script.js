a334f2af2b9c9ddce8d4fbca804df751d77b16d9
Aurelien Roux <aureroux13@me.com>
2017-02-09 17:56:17 -0500

    create link generator function

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var FLICKR_API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos
 var FLICKR_API_KEY = '0e20370658ae9b1a32d1f24c552061f8';
 var FLICKR_API_SEARCHTERM = 'guitar'
 
-
-
 function getPhotosForSearch(){
 	var url = `${FLICKR_API_URL}${FLICKR_API_KEY}&text=${FLICKR_API_SEARCHTERM}`;
 
@@ -14,24 +12,47 @@ function getPhotosForSearch(){
 		.then(result => result.json())
 		.then(function(jsonResponse){
 			var picsArray = jsonResponse.photos.photo;
-
 			picsArray.map(function(pic){
-				
 				var picObj = {
 					thumb: 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '_t.jpg',
 					large: 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '_h.jpg',
 					title: pic.title
 				}
-
 				return picObj;
-
 			})
 			.forEach(function(picLink){
 				console.log(picLink);
 			})
-
 		})
 	)
-	
 }
+
+function createFlickrThumb(photoData){
+	var link = document.createElement('a');
+	link.setAttribute('href', photoData.large);
+	link.setAttribute('target', '_blank');
+
+	var image = document.createElement("img");
+	image.setAttribute('src', photoData.thumb);
+	image.setAttribute('alt', photoData.title);
+
+	link.appendChild(image);
+
+	return link;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
