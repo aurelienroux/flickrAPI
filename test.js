441b32b152472224ebb9ff6a4eb5d44efc7d66e6
Aurelien Roux <aureroux13@me.com>
2017-02-11 12:57:01 -0500

    add query CDN link

---
+++ test.js
@@ -0,0 +1,51 @@
+var FLICKR_API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key='
+var FLICKR_API_KEY = '0e20370658ae9b1a32d1f24c552061f8';
+
+function getPhotosForSearch(searchTerm){
+	var url = `${FLICKR_API_URL}${FLICKR_API_KEY}&text=${searchTerm}`;
+	return(
+		fetch(url)
+		.then(result => result.json())
+		.then(function(jsonResponse){
+			var picsArray = jsonResponse.photos.photo;
+			return picsArray.map(function(pic){
+				var picObj = {
+					thumb: 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '_n.jpg',
+					large: 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '_h.jpg',
+					title: pic.title
+				}
+				return createFlickrThumb(picObj);
+			})
+		})
+	)
+}
+
+function createFlickrThumb(photoData){
+	var link = document.createElement('a');
+	link.setAttribute('href', photoData.large);
+	link.setAttribute('target', '_blank');
+	
+	link.style.background = `url(${photoData.thumb}) no-repeat center center`
+	link.style.backgroundSize = "cover";
+	link.style.minHeight = "200px";
+
+	return (link);
+}
+
+// UI COMPONENTS ////////////////////////////////////
+var app = document.querySelector('#app');
+var picForm = document.querySelector('.pic-form');
+var picTerm = document.querySelector('.pic-term');
+var picBtn = document.querySelector('.get-pic-btn');
+var picDisplay = document.querySelector('.pic-display');
+
+picForm.addEventListener('submit', function(event){
+	event.preventDefault();
+	var searchTerm = picTerm.value;
+
+	picDisplay.innerHTML = '';
+	getPhotosForSearch(searchTerm)
+	.then(arrayOfThumbnails => {
+		arrayOfThumbnails.forEach(pic => picDisplay.appendChild(pic))
+	})
+})
